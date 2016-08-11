2cc0a5954a78eb32ccfae26231d54543166b288e
Chris Sekira <cdsekira@msn.com>
2016-08-11 19:27:26 +0000

    added videos and fixed progressbar

--- js/default.js
+++ js/default.js
@@ -1,16 +1,23 @@
-function loadAndSetNavActive(activeItemId){
-	$("#myPageHeader").load("header.html", function(){
+function loadAndSetNavActive(activeItemId) {
+	$("#myPageHeader").load("header.html", function() {
 		$(activeItemId).attr("class", "active");
 	});
 }
 
-function loadFooter(){
-	$("#myPageFooter").load("footer.html");
+function loadFooter(loadProgressBar = false) {
+	$("#myPageFooter").load("footer.html", function() {
+		if (loadProgressBar == true) {
+			$("#pageloadProgress").parent().css("display", "block");
+		}
+	});
+
+
 }
 
-$("head").append($("<link/>",{
-	"rel":"icon",
-	"href":"cat_favicon.ico",
-	"type":"image/x-icon",
-	"sizes":"48x48"
+//set the favicon
+$("head").append($("<link/>", {
+	"rel": "icon",
+	"href": "cat_favicon.ico",
+	"type": "image/x-icon",
+	"sizes": "48x48"
 }));
--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	// get/call the default.js file to load headers then run custom functions
 	$.getScript("js/default.js", function() {
 		loadAndSetNavActive("#pageNav-Home");
-		loadFooter();
+		loadFooter(true);
 		
 		// custom functions
 		loadCatsToCarousel();
