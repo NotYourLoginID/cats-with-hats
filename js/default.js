abfa34f0b47f23bc02d75756d77bc3d333545d78
Chris Sekira <cdsekira@msn.com>
2016-08-11 17:27:56 +0000

    added favicon

--- js/default.js
+++ js/default.js
@@ -2,9 +2,15 @@ function loadAndSetNavActive(activeItemId){
 	$("#myPageHeader").load("header.html", function(){
 		$(activeItemId).attr("class", "active");
 	});
-	
 }
 
 function loadFooter(){
 	$("#myPageFooter").load("footer.html");
 }
+
+$("head").append($("<link/>",{
+	"rel":"icon",
+	"href":"cat_favicon.ico",
+	"type":"image/x-icon",
+	"sizes":"48x48"
+}));
