313758ee8a87db3fb9ca8087c18d5fd4572850c0
Chris Sekira <cdsekira@msn.com>
2016-08-11 21:42:13 +0000

    bug fixes and added geolocation

--- js/about.js
+++ js/about.js
@@ -3,7 +3,34 @@ $(document).ready(function() {
 	$.getScript("js/default.js", function() {
 		loadAndSetNavActive("#pageNav-About");
 		loadFooter();
-		
-		// custom functions
+		initMap();
+
 	});
 });
+
+
+function initMap() {
+	navigator.geolocation.getCurrentPosition(function(pos) {
+		var latlon = pos.coords.latitude + "," + pos.coords.longitude;
+		var img_url = "http://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&sensor=false&markers=color:blue|"+latlon;
+		$("#myMap").append($("<img/>", {
+			"src": img_url
+		}));
+	});
+}
+
+// function initMap() {
+// 	navigator.geolocation.getCurrentPosition(function(pos) {
+// 		var my_location = {
+// 			"lat": pos.coords.latitude,
+// 			"lon": pos.coords.longitude
+// 		};
+
+// 		var map = new google.maps.Map(document.getElementById('myMap'),{
+// 			zoom: 10,
+// 			center: my_location
+// 		});
+
+
+// 	});
+// }
